b7a611b196d055fc395311a3202faf79d628b8ff
AvhiMaz <avhimazumder5@outlook.com>
2025-11-30 16:51:32 +0530

    feat: add close stake account
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ programs/cirkle-contract/src/instructions/close_stake.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+use anchor_lang::system_program::{self, Transfer};
+
+#[derive(Accounts)]
+pub struct CloseStake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub city_mint: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            b"stake",
+            user.key().as_ref(),
+            city_mint.key().as_ref()
+        ],
+        bump
+    )]
+    pub user_stake: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> CloseStake<'info> {
+    pub fn close_stake(&self) -> Result<()> {
+        let lamports = self.user_stake.lamports();
+
+        let cpi_accounts = Transfer {
+            from: self.user_stake.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(
+            self.system_program.to_account_info(),
+            cpi_accounts,
+        );
+
+        system_program::transfer(cpi_ctx, lamports)?;
+
+        Ok(())
+    }
+}
